d07b06e4c52163884b63f77ab80d3500282a613a
Lee Jamison <ldjamison@marywood.edu>
2016-07-22 13:02:57 -0400

    Initial commit

---
+++ Koha/Plugin/Com/MarywoodUniversity/PALCIRedirect.pm
@@ -0,0 +1,86 @@
+package Koha::Plugin::Com::MarywoodUniversity::PALCIRedirect;
+
+## It's good practice to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Auth;
+use C4::Context;
+use C4::Output;
+
+## Here we set our plugin version
+our $VERSION = 1.00;
+
+## Here is our metadata. Some keys are required while some are optional.
+our $metadata = {
+	name	=> 'PALCI Redirect',
+	author	=> 'Lee Jamison',
+	description	=> 'This plugin enables PALCI members to leverage Koha LDAP authentication to login to PALCI E-ZBorrow',
+	date_authored	=> '2016-07-22',
+	date_updated	=> '2016-07-22',
+	minimum_version	=> '3.16',
+	maximum_version	=> undef,
+	version			=> $VERSION,
+};
+
+## This is the minimum code required for a plugin's 'new' method.
+## More can be added but none should be removed.
+sub new {
+	my ( $class, $args ) = @_;
+	
+	## We need to add our metadata here so our base class can access it.
+	$args->{'metadata'} = $metadata;
+	$args->{'metadata'}->{'class'} = $class;
+	
+	## Here, we call the 'new' method for our base class.
+	## This runs some additional magic and checking and
+	## returns our actual $self
+	my $self = $class->SUPER::new($args);
+	
+	return $self;
+}
+
+## The existance of a 'tool' subroutine means the plugin is capable
+## of running a tool. The difference between a tool and a report is
+## primarily semantic, but in general any plugin that modifies the
+## Koha database should be considered a tool.
+sub tool {
+	my ( $self, $args ) = @_;
+	
+	my $cgi = $self->{'cgi'};
+	
+	my $template = $self->get_template({ file => 'palci-redirect-tool.tt' });
+	
+	print $cgi->header();
+	print $template->output();
+}
+
+## If your tool is complicated enough to needs it's own setting/configuration
+## you will want to add a 'configure' method to your plugin like so.
+## Here all the logic is thrown into the configure method.
+sub configure {
+	my ( $self, $args ) = @_;
+	my $cgi = $self->{'cgi'};
+}
+
+## This is the 'install' method. Any database tables or other setup that should
+## be done when the plugin if first installed should be executed in this method.
+## The installation method should always return true if the installation succeeded
+## or false if it failed.
+sub install() {
+	my ( $self, $args ) = @_;
+	
+}
+
+## This method will be run just before the plugin files are deleted
+## when a plugin is uninstalled. It is good practice to clean up
+## after ourselves!
+sub uninstall() {
+	my ( $self, $args ) = @_;
+	
+}
+
+1;
---
+++ Koha/Plugin/Com/MarywoodUniversity/PALCIRedirect/palci-redirect.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+#
+# Copyright 2016 Marywood University
+#
+# This file is not part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+
+use C4::Auth;
+use C4::Context;
+use C4::Output;
+
+my $cgi = new CGI;
+
+my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
+	{
+		template_name	=> "palci-redirect.tt",
+		query			=> $cgi,
+		type			=> "opac",
+		authnotrequired	=> 1,
+		debug			=> 1,
+	}
+);
+
+## Relais institution code
+my $institutionCode = "MWOOD";
+
+## Base URL for E-ZBorrow NCIP Authentication
+my $baseURL = "https://e-zborrow.relaisd2d.com/service-proxy/?command=mkauth&LS=$institutionCode&PI=";
+
+## Currently logged in user's card number (barcode)
+my $cardNumber = C4::Context->userenv->{'cardnumber'};
+
+## Redirect the page to E-ZBorrow with current user's card number appended to the end of URL
+print $cgi->redirect( $baseURL . $cardNumber );
